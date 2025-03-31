481a426f6193a0687791747c440d5223987d910f
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-31 12:47:30 +0530

    9: displayed today's data section

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Right = () => {
+const Right = ({weatherData}) => {
   return (
     <div className='w-25'>
       <h3 className='pt-4 pb-3 text-secondary'>Today's Data</h3>
@@ -8,32 +8,32 @@ const Right = () => {
         <div className='d-flex justify-content-between fs-4 py-2'>
           <div className='d-flex justify-content-around'>
             <span><i class="fa-solid fa-temperature-high"> &nbsp;</i></span>
-            <span>08 &deg; C</span>
+            <span>{Math.round(weatherData?.main?.temp_max)} &deg; C</span>
           </div>
 
           <div>
             <span><i class="fa-solid fa-temperature-low"> &nbsp; </i></span>
-            <span>08 &deg; C</span>
+            <span>{Math.round(weatherData?.main?.temp_min)} &deg; C</span>
           </div>
         </div>
         <div className='d-flex justify-content-between fs-4 py-2'>
           <div>
             <span><i class="fa-solid fa-wind">&nbsp;&nbsp;</i></span>
-            <span>23</span>
+            <span>{weatherData?.wind?.speed}m/s</span>
           </div>
           <div>
             <span><i class="fa-solid fa-droplet">&nbsp;&nbsp;</i></span>
-            <span>55</span>
+            <span>{weatherData?.main?.humidity}g/m³</span>
           </div>
         </div>
         <div className='d-flex justify-content-between fs-5 py-2'>
             <span>Sunrise &nbsp;</span>
-            <span>09 : 00 AM</span>
+            <span>{new Date(weatherData?.sys?.sunrise).toLocaleTimeString("en-US")}</span>
         </div>
 
         <div className='d-flex justify-content-between fs-5 py-2'>
             <span>Sunset &nbsp;</span>
-            <span>09 : 00 AM</span>
+            <span>{new Date(weatherData?.sys?.sunset).toLocaleTimeString("en-US")}</span>
         </div>
 
       </div>
--- src/components/WeatherMain.jsx
+++ src/components/WeatherMain.jsx
@@ -31,7 +31,7 @@ const WeatherMain = () => {
     <div className='d-flex justify-content-center align-items-center vh-100 weatherMain'>
       <div className='d-flex w-75 shadow-lg rounded-5 leftRight'>
         <Left handleClick={handleClick} city={city} setCity={setCity} cities={cities} weatherData={weatherData} cityName={cityName}/>
-        <Right />
+        <Right weatherData={weatherData}/>
       </div>
     </div>
   )
