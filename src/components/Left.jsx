cbfa9a37987723b2891a0e42e864f8839ff2fe48
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-31 12:17:49 +0530

    9: displayed current temp, feels_like, city name

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Search from './Search'
 
-const Left = ({ handleClick, city, setCity, cities }) => {
+const Left = ({ handleClick, city, setCity, cities, weatherData, cityName }) => {
   return (
     <div className='w-75'>
       <div className='d-flex justify-content-between px-5 py-4'>
@@ -16,18 +16,18 @@ const Left = ({ handleClick, city, setCity, cities }) => {
 
       <div className='d-flex justify-content-center'>
         <div className='pe-5'>
-          <h1 className='temp text-light'>32 &deg;C</h1>
+          <h1 className='temp text-light'>{Math.round(weatherData?.main?.temp)} &deg;C</h1>
         </div> 
 
         <div className='ps-5'>
-          <img src="https://openweathermap.org/img/wn/09d@2x.png" alt="" height={120}/>
-          <h2 className='weatherDescription ps-4'>Rainy</h2>
-          <h5>Feels like 45 &deg; C</h5>
+          <img src={`https://openweathermap.org/img/wn/${(weatherData?.weather?.[0]?.icon)}@2x.png`} alt="" height={120}/>
+          <h2 className='weatherDescription '>{(weatherData?.weather?.[0]?.description)}</h2>
+          <h5>Feels like {Math.round(weatherData?.main?.feels_like)} &deg; C</h5>
         </div>
       </div>
 
       <div className='location'>
-          <h1 className='text-white'>Bhubaneswar</h1>
+          <h1 className='text-white'>{cityName}</h1>
       </div> 
 
       <h3 className='text-secondary ps-5'>Hourly Forecast</h3>
@@ -36,7 +36,7 @@ const Left = ({ handleClick, city, setCity, cities }) => {
         <div className='d-flex flex-column align-items-center rounded-3 hourlyDiv'>
           <p>12 : 00 AM</p>
           <p>08 &deg;C</p>
-          <img src="https://openweathermap.org/img/wn/09d@2x.png" alt="" />
+          {/* <img src={`https://openweathermap.org/img/wn/${(weatherData?.weather[0].icon)}@2x.png`} alt="" /> */}
           <p>Rainy</p>
         </div>
         <div className='d-flex flex-column align-items-center rounded-3 hourlyDiv'>
