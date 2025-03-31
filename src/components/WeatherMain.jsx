d8664e07ffd805cb839cc5a4148a51d936b8cbd0
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-31 14:26:31 +0530

    11: displayed hourly forecast

--- src/components/WeatherMain.jsx
+++ src/components/WeatherMain.jsx
@@ -10,12 +10,9 @@ const WeatherMain = () => {
   
   const weatherData = useSelector((state) => state.weather.weatherData)
   const timeData = useSelector((state) => state.weather.timeData.list)
-  console.log(timeData);
-  
+
   const getHourData = (timeData) => {
-    console.log(timeData);
-    
-    const filterData = timeData.filter((item) => 
+    const filterData = timeData?.filter((item) => 
       (new Date(item.dt_txt) > new Date()) && (new Date(item.dt_txt).getDate() === new Date().getDate())
     )  
     return filterData
@@ -42,13 +39,16 @@ const WeatherMain = () => {
     dispatch(fetchCities(city))
   }, [city])
 
-  
-  console.log(weatherData);
+  useEffect(() => {
+    dispatch(fetchWeather({lat: "20.2960", lon: "85.8246"}))
+    dispatch(fetchTimeData({lat: "20.2960", lon: "85.8246"}))
+  }, [])
+
     
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 weatherMain'>
       <div className='d-flex w-75 shadow-lg rounded-5 leftRight'>
-        <Left handleClick={handleClick} city={city} setCity={setCity} cities={cities} weatherData={weatherData} cityName={cityName}/>
+        <Left handleClick={handleClick} city={city} setCity={setCity} cities={cities} weatherData={weatherData} cityName={cityName} hourData={hourData}/>
         <Right weatherData={weatherData}/>
       </div>
     </div>
